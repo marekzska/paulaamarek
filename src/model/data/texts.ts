f865841204340085153018c8d180c79e6c4e0163
Marek Kiraly <marek@kiraly.sk>
2026-05-11 20:01:55 +0200

    program change

--- src/model/data/texts.ts
+++ src/model/data/texts.ts
@@ -85,7 +85,7 @@ Ak by vám tu niečo chýbalo alebo ste mali akékoľvek otázky, pokojne sa ná
   oficiálnom webe. V prípade, že nepôjdete z kostola s nami autobusom o
   parkovanie sa nemusíte báť. Priamo <b>pred bránami areálu</b> je miesto, kde
   môžete auto pohodlne zaparkovať.`,
-  whereCeremonyInfoText: `Parkovať môžete zboku kostola alebo pozdĺž cesty na ktoromkoľvek voľnom mieste. Prosíme, neparkujte na súkromných pozemkoch a dbajte, aby ste neblokovali vjazdy a výjazdy z domov. Ďakujeme za ohľaduplnosť.`,
+  whereCeremonyInfoText: `Parkovať môžete <b>zboku kostola alebo pozdĺž cesty</b> na ktoromkoľvek voľnom mieste. Prosíme, <b>neparkujte na súkromných pozemkoch</b> a dbajte, aby ste <b>neblokovali vjazdy a výjazdy</b> z domov. Ďakujeme za ohľaduplnosť.`,
   whereLinkText: "Stránka areálu",
 
   // footer section
