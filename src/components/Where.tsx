c95906257b420d624ca54f80ecbd65783c30e581
Marek Kiraly <marek@kiraly.sk>
2026-05-11 21:22:54 +0200

    program change

--- src/components/Where.tsx
+++ src/components/Where.tsx
@@ -4,12 +4,40 @@ import { Link } from "react-router";
 
 export const Where = () => {
   return (
+    
     <div className="mx-10 lg:px-10 py-20">
       <div className="flex flex-col gap-x-10 lg:grid grid-cols-2 gap-10 ">
         <div className="flex flex-col gap-4">
           <h2 className="text-darker font-bold text-7xl font-sansita">
             {texts.whereTitle}
           </h2>
+          <h3 className="text-darker font-bold text-3xl font-sansita">
+            {texts.whereTransportTitle}
+          </h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5307.333958111949!2d17.28849!3d48.309256!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c974638668f0d%3A0x9a8ff35e729e6bc3!2sKostol%20sv.%20Martina!5e0!3m2!1scs!2sch!4v1767792287108!5m2!1scs!2sch"
+            width="800"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="max-w-full"
+          ></iframe>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p
+            className="text-lighter text-base"
+            dangerouslySetInnerHTML={{ __html: texts.whereTransportInfoText }}
+          />
+        </div>
+      </div>
+      
+      <div className="flex flex-col gap-x-10 lg:grid grid-cols-2 gap-10 ">
+        <div className="flex flex-col gap-4">
+          {/* <h2 className="text-darker font-bold text-7xl font-sansita">
+            {texts.whereTitle}
+          </h2> */}
           <h3 className="text-darker font-bold text-3xl font-sansita">
             {texts.whereCeremonyTitle}
           </h3>
--- src/model/data/texts.ts
+++ src/model/data/texts.ts
@@ -80,11 +80,17 @@ Ak by vám tu niečo chýbalo alebo ste mali akékoľvek otázky, pokojne sa ná
   whereTitle: "Kde",
   whereCeremonyTitle: "Obrad",
   whereFeastTitle: "Hostina",
+  whereTransportTitle: "Autobus z Bratislavy do Vinosadov",
   whereInfoText: `
   Všetky informácie o areáli, kde sa svatba uskutoční nájdete na ich
   oficiálnom webe. V prípade, že nepôjdete z kostola s nami autobusom o
   parkovanie sa nemusíte báť. Priamo <b>pred bránami areálu</b> je miesto, kde
   môžete auto pohodlne zaparkovať.`,
+  whereTransportInfoText: `
+  <b>Miesto odchodu:</b> __________________ <br>
+  <b>Čas odchodu:<b> 13:45 <br>
+  Ak by sa chcel ešte niekto pridať, miest je dostatok — prosíme však, aby nám dal vopred vedieť, aby bus neodišiel bez neho.
+  `,
   whereCeremonyInfoText: `Parkovať môžete <b>zboku kostola alebo pozdĺž hlavnej cesty na ktoromkoľvek voľnom mieste</b>. Prosíme, <b>neparkujte na súkromných pozemkoch</b>, dbajte aby ste <b>neblokovali vjazdy a výjazdy</b> z domov a nenechávajte prosím auto pred kostolom, alebo v kopci na ceste ku kostolu. Ďakujeme za ohľaduplnosť.`,
   whereLinkText: "Stránka areálu",
 
