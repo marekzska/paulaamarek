a73b363a5de726143be540346d8a5d0b917a291c
Marek Kiraly <marek@kiraly.sk>
2026-05-11 21:24:42 +0200

    program change

--- src/model/data/texts.ts
+++ src/model/data/texts.ts
@@ -88,7 +88,7 @@ Ak by vám tu niečo chýbalo alebo ste mali akékoľvek otázky, pokojne sa ná
   môžete auto pohodlne zaparkovať.`,
   whereTransportInfoText: `
   <b>Miesto odchodu:</b> __________________ <br>
-  <b>Čas odchodu:<b> 13:45 <br>
+  <b>Čas odchodu:</b> 13:45 <br>
   Ak by sa chcel ešte niekto pridať, miest je dostatok — prosíme však, aby nám dal vopred vedieť, aby bus neodišiel bez neho.
   `,
   whereCeremonyInfoText: `Parkovať môžete <b>zboku kostola alebo pozdĺž hlavnej cesty na ktoromkoľvek voľnom mieste</b>. Prosíme, <b>neparkujte na súkromných pozemkoch</b>, dbajte aby ste <b>neblokovali vjazdy a výjazdy</b> z domov a nenechávajte prosím auto pred kostolom, alebo v kopci na ceste ku kostolu. Ďakujeme za ohľaduplnosť.`,
