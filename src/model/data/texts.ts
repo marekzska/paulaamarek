d7e13a49cdf038817c3c5d2b2c2d73d40b399cb0
Marek Kiraly <marek@kiraly.sk>
2026-05-11 18:49:27 +0200

    program change

--- src/model/data/texts.ts
+++ src/model/data/texts.ts
@@ -44,8 +44,7 @@ Ak by vám tu niečo chýbalo alebo ste mali akékoľvek otázky, pokojne sa ná
     <b>Krájanie torty</b> | 20:30 <br/><br/>
 
     <b>Čepčenie</b> | 22:30
-  `
-  ,
+    `,
 
   // MY sekcia
   usTitle: "My",
