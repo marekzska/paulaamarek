21a08813f9c7c0468289a7a2dd18342e08e0132e
Marek Kiraly <marek@kiraly.sk>
2026-05-11 21:26:18 +0200

    program change

--- src/components/Where.tsx
+++ src/components/Where.tsx
@@ -7,10 +7,10 @@ export const Where = () => {
     
     <div className="mx-10 lg:px-10 py-20">
       <div className="flex flex-col gap-x-10 lg:grid grid-cols-2 gap-10 ">
+        <h2 className="text-darker font-bold text-7xl font-sansita">
+          {texts.whereTitle}
+        </h2>
         <div className="flex flex-col gap-4">
-          <h2 className="text-darker font-bold text-7xl font-sansita">
-            {texts.whereTitle}
-          </h2>
           <h3 className="text-darker font-bold text-3xl font-sansita">
             {texts.whereTransportTitle}
           </h3>
