3ed5118072aaed4aadddc99ac662b35e1f6f6fa4
Marek Kiraly <marek@kiraly.sk>
2026-06-01 19:15:40 +0200

    pre-weddin edits

--- src/model/data/texts.ts
+++ src/model/data/texts.ts
@@ -90,7 +90,7 @@ Ak by vám tu niečo chýbalo alebo ste mali akékoľvek otázky, pokojne sa ná
   môžete auto pohodlne zaparkovať.`,
   whereTransportInfoText: `
   <b>Miesto odchodu:</b> <a href='https://maps.app.goo.gl/WpFRg4sMUNLSi6No6'> Dom Quo Vadis, Hurbanovo námestie 1</a><br>
-  <b>Čas odchodu:</b> 13:45 <br><br>
+  <b>Čas odchodu:</b> 13:15<br><br>
   <b>Ak by mal ešte niekto záujem o odvoz z BA, je veľmi pravdepodobné, že v autobuse budú volné miesta. Ozvite sa a odvoz dohodneme.</b>
   `,
   whereCeremonyInfoText: `Parkovať môžete <b>zboku kostola alebo pozdĺž hlavnej cesty na ktoromkoľvek voľnom mieste</b>. Prosíme, <b>neparkujte na súkromných pozemkoch</b>, dbajte aby ste <b>neblokovali vjazdy a výjazdy</b> z domov a nenechávajte prosím auto pred kostolom, alebo v kopci na ceste ku kostolu. Ďakujeme za ohľaduplnosť.`,
